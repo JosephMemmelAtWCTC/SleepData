69c18e78c34eef182471e724b414ea376e46357e
42null <averyparanoiduser@gmail.com>
2023-09-12 13:57:15 -0500

    Working logger and check for parse message option 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿string dataPath = "data.txt";
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "/nlog.config";
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+// See https://aka.ms/new-console-template for more information
+
+string dataPath = "data.txt";
 const int numPlacesInColumnAfterDividerDays = 2;
 const int numPlacesInColumnAfterDividerCaculated = 3;//Max is 3 with Average as "avg" in the code to match up with the example
 char underLinePattern = '-';
@@ -20,35 +27,38 @@ if (resp == "1")
     // ask a question
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
-    int weeks = int.Parse(Console.ReadLine());
-    // determine start and end date
-    DateTime today = DateTime.Now;
-    // we want full weeks sunday - saturday
-    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-    // subtract # of weeks from endDate to get startDate
-    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    // random number generator
-    Random rnd = new Random();
-    // create file
-    StreamWriter sw = new StreamWriter(dataPath);
+    if (int.TryParse(Console.ReadLine(), out int weeks)){
+        // determine start and end date
+        DateTime today = DateTime.Now;
+        // we want full weeks sunday - saturday
+        DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+        // subtract # of weeks from endDate to get startDate
+        DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+        // random number generator
+        Random rnd = new Random();
+        // create file
+        StreamWriter sw = new StreamWriter(dataPath);
 
-    // loop for the desired # of weeks
-    while (dataDate < dataEndDate)
-    {
-        // 7 days in a week
-        int[] hours = new int[7];
-        for (int i = 0; i < hours.Length; i++)
+        // loop for the desired # of weeks
+        while (dataDate < dataEndDate)
         {
-            // generate random number of hours slept between 4-12 (inclusive)
-            hours[i] = rnd.Next(4, 13);
+            // 7 days in a week
+            int[] hours = new int[7];
+            for (int i = 0; i < hours.Length; i++)
+            {
+                // generate random number of hours slept between 4-12 (inclusive)
+                hours[i] = rnd.Next(4, 13);
+            }
+            // M/d/yyyy,#|#|#|#|#|#|#
+            // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+            sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+            // add 1 week to date
+            dataDate = dataDate.AddDays(7);
         }
-        // M/d/yyyy,#|#|#|#|#|#|#
-        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-        // add 1 week to date
-        dataDate = dataDate.AddDays(7);
+        sw.Close();
+    }else{
+        logger.Error("You must enter a valid number");
     }
-    sw.Close();
 }
 else if (resp == "2")
 {
